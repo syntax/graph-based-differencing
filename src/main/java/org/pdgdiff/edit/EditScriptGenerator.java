71d8befbe534e30e2afe40544a2cc31fe09c12a4
syntax <tfeholland@gmail.com>
2024-10-29 16:49:47 +0000

    fix method signature differencing

--- src/main/java/org/pdgdiff/edit/EditScriptGenerator.java
+++ src/main/java/org/pdgdiff/edit/EditScriptGenerator.java
@@ -5,6 +5,7 @@ import org.pdgdiff.matching.GraphMapping;
 import org.pdgdiff.matching.NodeMapping;
 import org.pdgdiff.util.SourceCodeMapper;
 import soot.SootMethod;
+import soot.Type;
 import soot.Unit;
 import soot.tagkit.LineNumberTag;
 import soot.toolkits.graph.Block;
@@ -14,6 +15,8 @@ import soot.toolkits.graph.pdg.PDGNode;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Generates edit scripts based on PDG node mappings.
@@ -96,10 +99,8 @@ public class EditScriptGenerator {
         String newMethodSignature = destMethod.getDeclaration();
 
         if (!oldMethodSignature.equals(newMethodSignature)) {
-            // TODO: this is a bit of a hacky solution (made by soot) and will need to be improve. Just -1 from the first line
-            // TODO: of the body to get the method signature line number, but comments/whitespace can be above it so this is naive.
-            int oldMethodLineNumber = srcMethod.getJavaSourceStartLineNumber();
-            int newMethodLineNumber = destMethod.getJavaSourceStartLineNumber();
+            int oldMethodLineNumber = getMethodLineNumber(srcMethod, srcCodeMapper);
+            int newMethodLineNumber = getMethodLineNumber(destMethod, dstCodeMapper);
 
             Update signatureUpdate = new Update(
                     null, // no specific PDGNode associated for signature update
@@ -136,6 +137,61 @@ public class EditScriptGenerator {
             this.syntaxDifferences = syntaxDifferences;
         }
     }
+
+    public static int getMethodLineNumber(SootMethod method, SourceCodeMapper srcCodeMapper) throws IOException {
+        int currentLine = method.getJavaSourceStartLineNumber();
+
+        String methodName = method.getName();
+        String returnType = method.getReturnType().toString();
+        List<Type> parameterTypes = method.getParameterTypes();
+
+        // regex pattern to match the method declaration
+        String methodPattern = buildMethodPattern(returnType, methodName, parameterTypes);
+        Pattern pattern = Pattern.compile(methodPattern);
+        StringBuilder accumulatedLines = new StringBuilder();
+        int methodDeclarationLine = -1;
+
+        for (int i = 0; i < 20 && currentLine > 0; currentLine--) {
+            String line = srcCodeMapper.getCodeLine(currentLine).trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            accumulatedLines.insert(0, line + " ");
+
+            // checking if these match the regex per that method. (this is a Java.Regex matcher, not one of mine
+            // naming a big confusing
+            Matcher regexMatcher = pattern.matcher(accumulatedLines.toString());
+            if (regexMatcher.find()) {
+                methodDeclarationLine = currentLine;
+                break;
+            }
+        }
+
+        return methodDeclarationLine != -1 ? methodDeclarationLine : currentLine;
+    }
+
+    // TODO: refactor all the method stuff into its own class, cos this si getting huge
+    private static String buildMethodPattern(String returnType, String methodName, List<Type> parameterTypes) {
+        StringBuilder paramsPattern = new StringBuilder();
+        paramsPattern.append("\\(");
+        for (int i = 0; i < parameterTypes.size(); i++) {
+            paramsPattern.append(".*");
+            if (i < parameterTypes.size() - 1) {
+                paramsPattern.append(",");
+            }
+        }
+        paramsPattern.append("\\)");
+
+        String methodPattern = String.format(
+                ".*\\b%s\\b\\s+\\b%s\\b\\s*%s.*",
+                Pattern.quote(returnType),
+                Pattern.quote(methodName),
+                paramsPattern
+        );
+        return methodPattern;
+    }
+
 
     public static int getNodeLineNumber(PDGNode node) {
         if (node.getType() == PDGNode.Type.CFGNODE) {
